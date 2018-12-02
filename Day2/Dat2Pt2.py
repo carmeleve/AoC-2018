6710cff614f45e84afa85a563cfec5a73cc9c1da
Carmel Eve <Carmel.Eve@endjin.com>
2018-12-02 17:50:00 +0000

    Corrected spelling

--- Day2/Dat2Pt2.py
+++ Day2/Dat2Pt2.py
@@ -2,8 +2,8 @@ import csv
 
 def remove_char(str, n):
       first_part = str[:n] 
-      last_pasrt = str[n+1:]
-      return first_part + last_pasrt
+      last_part = str[n+1:]
+      return first_part + last_part
 
 filePath = r"C:\Users\CarmelEve\Documents\GitHub\AoC-2018\Day2\input.csv"
 
